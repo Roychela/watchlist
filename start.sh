f8aa4298af5bc0f0c06e5a0dd21fa579e1240e5f
Roy Chela <roychela@gmail.com>
2019-06-20 13:13:33 +0300

    deployment to heroku

---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='0a9810c26bf8c8d73ad820eb1441835c'
+export SECRET_KEY='<Flask WTF Secret Key>'
+
+python3.6 manage.py server
---
+++ tests/test_review.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Review
+
+class ReviewTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_review = Review(1234,'Python Must Be Crazy','imageurl.com','Great movie')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_review,Review))
