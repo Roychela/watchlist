9bea0b53b243b30e2fcd68261b813787bf9be9e0
Roy Chela <roychela@gmail.com>
2019-06-30 12:55:23 +0300

    Deploy to heroku

--- config.py
+++ config.py
@@ -5,18 +5,31 @@ class Config:
     '''
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY') 
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Watchlist'
+    SENDER_EMAIL = 'roychela@gmail.com'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
     '''
     Production  configuration child class
 
     Args:
-        Config: The parent configuration class with General configuration settings
+        Config: The parent configuration class with General configuration settings   
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roy:1234567@localhost/watchlist_test'
 
 class DevConfig(Config):
     '''
@@ -24,12 +37,15 @@ class DevConfig(Config):
 
     Args:
         Config: The parent configuration class with General configuration settings
+
     '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://roy:1234567@localhost/watchlist'
 
     DEBUG = True
 
 
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MOVIE_API_KEY='0a9810c26bf8c8d73ad820eb1441835c'
 export SECRET_KEY='<Flask WTF Secret Key>'
-
+export MAIL_USERNAME='roychela@gmail.com'
+export MAIL_PASSWORD='1234123456'
 python3.6 manage.py server
